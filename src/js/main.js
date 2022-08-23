8051fb5e3c1b87ea11c7776fa7d8409129cf8a62
eliaspeschke <eliaspeschke@googlemail.com>
2022-08-23 11:58:21 +0200

    lr

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@
 import LocomotiveScroll from 'locomotive-scroll';
 
 
+//const video_sources = ['./dots.mp4', './knee1.mp4', './knee2.mp4'];
 
 window.addEventListener('load', function () {
     const scroll = new LocomotiveScroll({
@@ -11,14 +12,41 @@ window.addEventListener('load', function () {
     });
 
     var vidProg = 0.0;
-    const video = document.querySelector('#background-video');
+    const video0 = document.querySelector('#video-0');
+    const video1 = document.querySelector('#video-1');
+    const video2 = document.querySelector('#video-2');
+    var videos = [video1, video2, video0];
+    var video = video0;
+    const btnNext = this.document.querySelector('#next');
+    const btnPrev = this.document.querySelector('#prev');
+
+
+    btnNext.addEventListener('click', (args) => {
+        console.log(args);
+        video.classList.remove('video-current');
+        video.classList.add('video-queued');
+        video = videos.shift();
+        videos.push(video);
+        video.classList.remove('video-queued');
+        video.classList.add('video-current');
+    });
+
+    btnPrev.addEventListener('click', (args) => {
+        console.log(args);
+        video.classList.remove('video-current');
+        video.classList.add('video-queued');
+        video = videos.pop();
+        videos.unshift(video);
+        video.classList.remove('video-queued');
+        video.classList.add('video-current');
+    });
     
     scroll.on('scroll', (args) => {
         // Get all current elements : args.currentElements
-        console.log(args);
+        //console.log(args);
         if(typeof args.currentElements['el0'] === 'object') {
             let progress = args.currentElements['el0'].progress;
-            console.log(progress);
+            //console.log(progress);
             vidProg = progress;
             video.currentTime = video.duration * vidProg;
             // ouput log example: 0.34
